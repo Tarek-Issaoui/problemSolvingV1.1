bf013da78ab00df1f8f6063f9b99e0d92f38cc8d
mehdi_jaouedi <mehdijaouedi9@gmaile.com>
2023-08-29 21:54:50 +0100

    done problem 1

--- problem1.js
+++ problem1.js
@@ -1,19 +1,26 @@
 /**
  *? Alphabet Inventory
  *? Given an alphabet and a string of text, write a method that tallies the count of each letter defined in said alphabet (case insensitive), then return the result of this tally.
- *? alphaCount("aBc", "aabbccdd") // "a:2,b:2,c:2"
+ *? alphaCount("aBc", "aaccdd") // "a:2,b:2,c:2"
  *? alphaCount("x", "Racer X is my friend") //	"x:1"
  *? alphaCount("123", "o hai") // "no matches"
  */
 const alphaCount = (alphabet, text) => {
-    let store = {};
-    let count = 0
-    for (let e of alphabet) {
-        for (let e1 of text) {
-            e1 === e && count++; 
-            store[e.toLowerCase()] = count
-        }
+  let store = {};
+  let count = 0;
+  for (let e of alphabet) {
+    for (let e1 of text) {
+      e1.toLowerCase() === e.toLowerCase() && count++;
 
+      store[e.toLowerCase()] = count;
     }
-    return JSON.stringify(store).slice(1, JSON.stringify(store).length - 1)
+    count = 0;
+  }
+  if (Object.values(store).reduce((acc, elem) => acc + elem) === 0) {
+    return "no match ";
+  }
+  return `"${JSON.stringify(store)
+    .slice(1, JSON.stringify(store).length - 1)
+    .replaceAll(/"/gi, "")}"`;
 };
+console.log(alphaCount("aBc", "aaccdd"));
